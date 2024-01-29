09e1e7bdc11ead77d441632806865cdf38aab9f3
Bawnorton <bawnorton@me.com>
2024-01-29 14:10:24 -0800

    Warn on old-target class matching

--- src/main/java/com/bawnorton/mixinsquared/canceller/MixinCancellerRegistrar.java
+++ src/main/java/com/bawnorton/mixinsquared/canceller/MixinCancellerRegistrar.java
@@ -7,13 +7,22 @@ import org.spongepowered.asm.service.MixinService;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public abstract class MixinCancellerRegistrar {
     private static final Set<MixinCanceller> cancellers = new HashSet<>();
     private static final ILogger LOGGER = MixinService.getService().getLogger("mixinsquared");
 
     public static boolean shouldCancel(List<String> targetClassNames, String mixinClassName) {
-        return cancellers.stream().anyMatch(canceller -> canceller.shouldCancel(targetClassNames, mixinClassName));
+        return cancellers.stream().anyMatch(canceller -> {
+            if(canceller.shouldCancel(targetClassNames, mixinClassName)) {
+                return true;
+            } else if(canceller.shouldCancel(targetClassNames.stream().map(s -> s.replaceAll("\\.", "/")).collect(Collectors.toList()), "")) {
+                LOGGER.warn("Canceller {} is matching with a deprecated target class format, use \".\" instead of \"/\"", canceller.getClass().getName());
+                return true;
+            }
+            return false;
+        });
     }
 
     public static void register(MixinCanceller canceller) {
